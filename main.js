ef8cd77ecdc925cd9a9ea3a08e0ad5bbd03767da
Gana Ramachandra <ganapathi.r05@gmail.com>
2017-02-16 18:41:38 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
-datasetTotal = {
-    "1": ["www.youtube.com", "9.56 KB", "47.9"],
-    "2": ["accounts.youtube.com", "7.74 KB", "38.8"],
-    "3": ["portal.mockcompany.com", "2.67 KB", "13.4"],
-    "4": ["ds-aksb-a.akamaihd.net", "0.00 B", "0.0"],
-    "5": ["hp.mockcompany.com", "0.00 B", "0.0"],
-};
+datasetTotal = [
+    { label: 'Category 1', value: 19 },
+    { label: 'Category 2', value: 5 },
+    { label: 'Category 3', value: 13 },
+    { label: 'Category 4', value: 17 },
+    { label: 'Category 5', value: 19 },
+    { label: 'Category 6', value: 27 }
+];
 
-datasetOption1 = {
-    "1": ["source.functional.mockcompany.com", "808.57 MB", "93.7"],
-    "2": ["devops.mockcompany.com", "20.21 MB", "2.3"],
-    "3": ["cnnios-f.akamaihd.net", "14.24 MB", "1.7"],
-    "4": ["espnfw-a.akamaihd.net", "6.43 MB", "0.7"],
-    "5": ["gm1.ggpht.com", "4.80 MB", "0.6"],
-    "6": ["portal.mockcompany.com", "2.09 MB", "0.2"],
-    "7": ["i.cdn.cnn.com", "1.28 MB", "0.1"],
-    "8": ["172.31.28.190", "1.13 MB", "0.1"],
-    "9": ["www.youtube.com", "972.87 KB", "0.1"],
-    "10": ["r4---sn-nx5e6n7s.googlevideo.com", "827.14 KB", "0.1"]
-};
+datasetOption1 = [
+    { label: "Category 11", value: 22 },
+    { label: "Category 12", value: 33 },
+    { label: "Category 13", value: 4 },
+    { label: "Category 14", value: 15 },
+    { label: "Category 15", value: 36 },
+    { label: "Category 16", value: 0 }
+];
 
-datasetOption2 = {
-    "1": ["vmahajan@mockcompany.com", "773.61 MB", "89.4"],
-    "2": ["jchanak@mockcompany.com", "75.34 MB", "8.7"],
-    "3": ["testuser7@mockcompany.com", "7.93 MB", "0.9"],
-    "4": ["asingh@mockcompany.com", "4.66 MB", "0.5"],
-    "5": ["pdesai@mockcompany.com", "4.25 MB", "0.5"],
-    "6": ["testuser1@mockcompany.com", "17.29 KB", "0.0"],
-    "7": ["testuser4@mockcompany.com", "201.00 B", "0.0"],
-    "8": ["hpadhye@mockcompany.com", "0.00 B", "0.0"]
-};
+datasetOption2 = [
+    { label: "Category 21", value: 10 },
+    { label: "Category 22", value: 20 },
+    { label: "Category 23", value: 30 },
+    { label: "Category 24", value: 5 },
+    { label: "Category 25", value: 12 },
+    { label: "Category 26", value: 23 }
+];
 
 d3.selectAll("input").on("change", selectDataset);
 
@@ -51,7 +46,7 @@ d3.selectAll("input").on("change", selectDataset);
 
 
 
-var margin = 60,
+var margin = 40,
     valueMargin = 12,
     labelMargin = 25,
     width = parseInt(d3.select('body').style('width'), 10),
@@ -70,10 +65,8 @@ svg = d3.select('body')
 
 function updateData(newData) {
 
-    newData = Object.values(newData);
-
     max = d3.max(newData, function(d) {
-        return parseInt(d[2]);
+        return d.value;
     });
 
 
@@ -98,7 +91,7 @@ function updateData(newData) {
                         .attr('dx', valueMargin) //margin right
                         .attr('dy', '25') //vertical align middle
                         .text(function(d) {
-                            return d[1];
+                            return d.value + '%';
                         }).each(function() {
                             labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
                         });
@@ -111,7 +104,7 @@ function updateData(newData) {
         .attr('width', 0) //for animation purpose; start with width=0
         .transition().duration(750)//time in ms
         .attr('width', function(d) {
-            return scale(parseInt(d[2]));
+            return scale(d.value);
         });
 
     var barTexts = barEnter.append('text')
@@ -123,7 +116,7 @@ function updateData(newData) {
         .attr('dy', '25') //vertical align middle
         .attr('text-anchor', 'start')
         .text(function(d) {
-            return (d[0]);
+            return (d.label);
         });
 
     // updated data:
@@ -135,7 +128,7 @@ function updateData(newData) {
         .attr('dx', valueMargin) //margin right
         .attr('dy', '25') //vertical align middle
         .text(function(d) {
-            return d[1];
+            return d.value + '%';
         }).each(function() {
             labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
         });
@@ -147,7 +140,7 @@ function updateData(newData) {
         })
         .attr('height', barHeight)
         .attr('width', function(d) {
-            return scale(parseInt(d[2]));
+            return scale(d.value);
         });
 
     var barTextsUpdate = bar.select('.value').transition()
@@ -159,7 +152,7 @@ function updateData(newData) {
         .attr('dy', '25') //vertical align middle
         .attr('text-anchor', 'start')
         .text(function(d) {
-            return (d[0]);
+            return (d.label);
         });
 
     
